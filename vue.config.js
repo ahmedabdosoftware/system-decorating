f9fbc187045117542c70131375c2878aac18be50
ahmed <ahmedabdosoftware@gmail.com>
2025-05-20 21:57:08 +0300

    Edit Config For custom domain

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
+// Before Custom Domain With GitHub Pages
+// module.exports = {
+//   publicPath:
+//     process.env.NODE_ENV === "production" ? "/system-decorating/" : "/",
+// };
 
+// After Custom Domain With GitHub Pages
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? 'https://ahmedabdosoftware.github.io/system-decorating/'
     : '/'
 }
+
